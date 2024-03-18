ebc47350c332c100486c2c321f05592b9e6147ac
Egor <des2107@mail.ru>
2024-03-19 00:09:17 +0300

    add migrations

---
+++ db/migration/000001_init_schema.up.sql
@@ -0,0 +1,39 @@
+
+create table actors
+(
+    id      serial
+        primary key,
+    name    varchar(50),
+    gender  varchar(15),
+    birhday varchar(15)
+);
+
+create table films
+(
+    id          serial
+        primary key,
+    name        varchar(150),
+    description varchar(1000),
+    date        varchar(8),
+    rating      integer
+);
+
+create table actors_films
+(
+    actor_id integer not null
+        references actors
+            on update cascade,
+    film_id  integer not null
+        references films
+            on update cascade on delete cascade,
+    primary key (actor_id, film_id)
+);
+
+create table users
+(
+    id      serial
+        primary key,
+    role    varchar(15),
+    api_key varchar(150)
+);
+
